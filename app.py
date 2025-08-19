9830d437860552e4920324507fc66ecffdd252dc
artista22 <gitlab.hunter242@aleeas.com>
2025-08-19 08:33:50 -0500

    FIX: se agrego configuración de puerto

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.secret_key="develoteca"
 
 # configuración de la base de datos
 app.config['MYSQL_HOST']='127.0.0.1'
+app.config['MYSQL_PORT']= 3306  # configuración de puerto
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='demo_db'
