67cda9a2b6189e2b7ab8ace64dc82a40e8c8499a
artista22 <gitlab.hunter242@aleeas.com>
2025-09-07 10:35:13 -0500

    tbl_usuarios

--- script_demo_db.sql
+++ script_demo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 19, 2025 at 03:37 PM
+-- Generation Time: Sep 07, 2025 at 05:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
